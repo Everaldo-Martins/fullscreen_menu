6892381f65d8ef07ac817861a50e815fb9f7b145
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2024-05-20 14:45:55 -0300

    Update project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
-const body = document.querySelector('body'),
-    container = document.querySelector('.js-menu-container'),
+const container = document.querySelector('.js-menu-container'),
     open_menu = document.querySelector('.js-menu-button'),
     close_menu = document.querySelector('.js-menu-close');
 
 function openMenu() {
     container.classList.add("is-open");
-    body.style.overflow = 'hidden';;
 }
 function closeMenu() {
     container.classList.remove("is-open");
-    body.style.overflow = 'auto';
 }
 open_menu.onclick = function() {
     openMenu();
